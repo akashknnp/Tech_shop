2fe12fc377d486f44be607553d778e389bf0921f
Akash N N <akashnn2003@gmail.com>
2026-01-06 12:51:06 +0530

    designed the sliders , worked on stylings

--- src/Components/Sliders.jsx
+++ src/Components/Sliders.jsx
@@ -2,36 +2,72 @@ import React, { useEffect, useState } from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Navigation, Pagination, Autoplay } from 'swiper/modules'
 import productsData from '../Data/productsData'
+import './Sliders.css'
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+
 
 const Sliders = () => {
 
     const [sliderData, setsliderData] = useState([])
 
     useEffect(() => {
-        setsliderData(productsData.slice(0, 3))
+        sliders()
         console.log(sliderData);
+
     }, [])
 
+    const sliders = () => {
+        setsliderData(productsData.filter((product) => {
+            return Object.keys(product).includes("heroImage")
+        }))
+    }
 
     return (
         <>
             {
-                sliderData ? (
-                    <Swiper navigation
-                        pagination={{ clickable: true }}
-                        autoplay={{ delay: 2500 }}>
-                        {
-                            sliderData.map((prod) => (
-                                <SwiperSlide>
-                                    <h2>{prod.title}</h2>
-                                    <img src="prod.images[0]" />
+                sliderData.length > 0 ? (
+                    <div className='slider'>
+                        <div className='bg-slider'>
+                            <h1>Over the <br />sound</h1>
+                        </div>
+                        <Swiper navigation
+                            pagination={{ clickable: true }}
+                            autoplay={{ delay: 2800 }}
+                            spaceBetween={20}
+                            slidesPerView={1}
+                            slidesPerGroup={1}
+                            loop={true}
+                            modules={[Navigation, Pagination, Autoplay]} className='swiper'>
+                            {
+                                sliderData.map((prod) => (
+                                    < SwiperSlide >
+                                        <div className='cards'>
+                                            <div className='slide-div-1'>
+                                                <h2>{prod.title}</h2>
+                                                <br /><br />
+                                                <br />
+                                                <h1>{prod.tagline}</h1>
+                                                <br /><br />
+                                                <div className='price'>
+                                                    <p>${prod.finalPrice}</p>
+                                                    <p>${prod.originalPrice}</p>
+                                                </div>
+                                                <button>Shop Now</button>
+                                            </div>
+                                            <div className='slide-div-2'>
+                                                <img src={prod.heroImage} />
+                                            </div>
+                                        </div>
+                                    </SwiperSlide>
+                                ))
+                            }
+                        </Swiper >
 
-                                </SwiperSlide>
-                            ))
-                        }
-
-                    </Swiper>
-                ) : (<h2>Loading . . .</h2>)
+                    </div >
+                ) : (<>loading</>)
             }
 
         </>
