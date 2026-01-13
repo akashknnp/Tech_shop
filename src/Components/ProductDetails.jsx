c86e24a5ff62ff4355e3242cf9921b0e2919e9d6
Akash N N <akashnn2003@gmail.com>
2026-01-14 00:06:25 +0530

    Added related products component under the Details of product, worked on specifications

--- src/Components/ProductDetails.jsx
+++ src/Components/ProductDetails.jsx
@@ -1,32 +1,76 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import productsData from '../Data/productsData'
 import { FaStar } from "react-icons/fa";
 import Dstyle from '../Components/ProductDetails.module.css'
 import { useDispatch } from 'react-redux';
 import { addTocart } from '../Redux/CartSlice';
+import reviewsData from '../Data/reviewsData';
+import Footer from '../Components/Footer'
+import Advantages from '../Components/Advantages'
 
 const ProductDetails = () => {
+    const navigate = useNavigate()
     const [activeimage, setActiveimage] = useState()
     const dispatch = useDispatch()
     const { Pid } = useParams()
     const [cbtn, setCbtn] = useState(false)
+    const [Specify, setSpecify] = useState(true)
+    const [overview, setOverview] = useState(false)
+    const [review, setReview] = useState(false)
+
+    const [cartbtn, setbtn] = useState(null)
 
 
     const product = productsData.find((item) => {
         return item.id === Number(Pid)
     })
 
+    const relatedItems = productsData.filter((item) => {
+        return product.category === item.category
+    })
+
     const handledetail = (prod) => {
-
-
         dispatch(addTocart(prod))
         setCbtn(true)
-
         setTimeout(() => {
             setCbtn(false)
         }, 2000)
     }
+    const handleCart = (param) => {
+        console.log(param);
+        setbtn(param.id)
+        dispatch(addTocart(param))
+
+        setTimeout(() => {
+            setbtn(null)
+        }, 2000)
+    }
+    const handleDetails = (itemId) => {
+        navigate(`/product/${itemId}`)
+    }
+
+
+    const handletoggle = (param) => {
+        if (param === 'overview') {
+            setOverview(true)
+            setReview(false)
+            setSpecify(false)
+        }
+        else if (param === 'review') {
+            setReview(true)
+            setSpecify(false)
+            setOverview(false)
+        }
+        else {
+            setSpecify(true)
+            setOverview(false)
+            setReview(false)
+        }
+    }
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [product]);
 
     return (
         <>
@@ -98,6 +142,132 @@ const ProductDetails = () => {
                 }
 
             </div >
+
+
+            <div className={Dstyle.Seconddiv}>
+                <div className={Dstyle.menuDetails}>
+                    <p onClick={() => handletoggle('specify')}>Specifications</p>
+                    <p onClick={() => handletoggle('overview')}>Overview</p>
+                    <p onClick={() => handletoggle('review')}>Review</p>
+                </div>
+                <div>
+                    {
+                        Specify && (
+                            <div className={Dstyle.specifyDetails}>
+                                <div>
+                                    <p>Brand</p>
+                                    <p>{product.brand}</p>
+                                </div>
+                                <div>
+                                    <p>Model</p>
+                                    <p>{product.title}</p>
+                                </div>
+                                <div>
+                                    <p>Generic Name</p>
+                                    <p>{product.category}</p>
+                                </div>
+                                <div>
+                                    <p>HeadPhone Type</p>
+                                    <p>{product.type}</p>
+                                </div>
+                                <div>
+                                    <p>Connectivity</p>
+                                    <p>{product.connectivity}</p>
+                                </div>
+                                <div>
+                                    <p>Microphone</p>
+                                    <p>Yes</p>
+                                </div>
+                            </div>
+                        )
+                    }
+                </div>
+                <div className={Dstyle.overviews}>
+                    {
+                        overview && (
+                            <div>
+                                <h3>The <b color='red'>{product.title} {product.info}</b> provides with fabulous ans Sound quality</h3>
+                                <ul>Sound Tunned to Perfection</ul>
+                                <ul>Comfortable to Wear</ul>
+                                <ul>Long Hours Playback Time</ul>
+                                <p>Buy the <b><i> {product.title} {product.info} </i></b> which offers you with fabulous music experience by providing you with awesome sound quality that you can never move on from. Enjoy the perfect flexibility and mobility with amazing music experience and quality with these neckbank giving you a truly awesome audio experience.</p>
+                            </div>
+                        )
+                    }
+                </div>
+                <div>
+                    {
+                        review && (
+                            <div>
+                                {
+
+                                    reviewsData.map((item) => (
+                                        <div className={Dstyle.reviewsDetails}>
+                                            <h4>{item.name}</h4>
+                                            <div>
+                                                {
+                                                    [...Array(item.rateCount)].map((_, i) => (
+                                                        <FaStar key={i} color="red" />
+                                                    ))
+                                                }
+                                                <p>{item.date}</p>
+                                            </div>
+
+                                            <p>{item.review}</p>
+                                        </div>
+                                    ))
+                                }
+
+                            </div>
+                        )
+                    }
+                </div>
+                <div className={Dstyle.relatedprod}>
+                    <h1> - - Related Products - -</h1>
+
+                    <div>
+                        {
+                            <div className={Dstyle.productMain}>
+                                {
+                                    relatedItems.length > 0 ? (
+                                        relatedItems.map((item) => (
+                                            <div className={Dstyle.item} key={item.id}>
+                                                <div onClick={() => handleDetails(item.id)}>
+                                                    <img src={item.images[0]} />
+                                                </div>
+                                                <div>
+                                                    {
+                                                        [...Array(item.rateCount)].map((_, i) => (
+                                                            <FaStar key={i} color="red" />
+                                                        ))
+                                                    }
+
+                                                </div>
+                                                <br />
+                                                <h2>{item.title}</h2>
+                                                <p>{item.info}</p>
+                                                <br /><hr /><br />
+                                                <div>
+                                                    <h1>₨ {item.finalPrice}</h1>
+                                                    <h4>₨ {item.originalPrice}</h4>
+
+                                                </div>
+                                                <button onClick={() => handleCart(item)}>
+                                                    {cartbtn === item.id ? <p className={Dstyle.sucesbtn}>Added</p> : "Add to cart"}
+                                                </button>
+
+                                            </div>
+                                        ))
+                                    ) : (<>Loading</>)
+                                }
+
+                            </div>
+                        }
+                    </div>
+                </div>
+            </div>
+            <Advantages />
+            <Footer />
         </>
     )
 }
